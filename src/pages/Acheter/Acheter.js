542736e85d2c05db5dcfb2b94fac8b6c3788eff7
kmaraab <aboubacarcamara0110@gmail.com>
2024-02-01 23:00:18 +0100

    ajout de la section "Investire dans l'immobilier"

--- src/pages/Acheter/Acheter.js
+++ src/pages/Acheter/Acheter.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import FirstSectionPage from '../../components/FirstSectionPage/FirstSectionPage'
 import Image from '../../assets/images/united-states-spokane.jpg'
+import { Link } from 'react-router-dom'
+import styles from './Acheter.module.scss'
 
 function Acheter() {
   return (
@@ -10,6 +12,19 @@ function Acheter() {
         title='Acheter un bien'
         description='Votre havre de paix vous attend dans cette élégante maison, prête à devenir vôtre.'
       />
+
+      <div className={styles.investerSection}>
+        <h2>INVESTIR DANS L’IMMOBILIER</h2>
+        <ul>
+          <li>Accompagnement, étude de rentabilité.</li>
+          <li>Recherche de biens en France et au Sénégal.</li>
+          <li>
+            Sénégal, destination soleil durant toute l’année, pour vous dénicher
+            le bien qui correspond à vos attentes.
+          </li>
+        </ul>
+        <Link to='nous-contacter'>Nous contacter</Link>
+      </div>
     </div>
   )
 }
