2ed69938c63e5df5abb9f39cf3fd9f5d4e26c99b
kmaraab <aboubacarcamara0110@gmail.com>
2024-02-11 09:25:58 +0100

    modif de style page a propos

--- src/pages/A_Propos/A_Propos.js
+++ src/pages/A_Propos/A_Propos.js
@@ -17,27 +17,27 @@ function A_Propos() {
 
         <div className={styles.textContent}>
           <p className={styles.borderStyle}></p>
-          Commerciale depuis une trentaine d’années sur le territoire national
-          et en particulier en Bourgogne Franche-Comté, je suis reconnue pour
-          mon professionnalisme, mon sens aigu du service et des relations
-          humaines.
-          <br />
-          <br />
-          Aujourd’hui, je mets mon savoir-faire à votre service en créant Marli
-          Immobilier. Avec audace et efficacité je vous propose des services à
-          la mesure de vos attentes.
-          <br />
-          <br />
-          Comme dans toutes les fonctions que j’ai eues à occuper jusqu’ici, je
-          ne lésinerai pas sur les moyens pour mener à bien les missions que
-          vous me confierez.
-          <br />
-          <br />
-          Je me suis entourée de personnes expertes dans leur domaine d’activité
-          pour vous garantir un accompagnement optimal. Merci de votre
-          confiance.
-          <br />
-          <br />
+          <p>
+            Commerciale depuis une trentaine d’années sur le territoire national
+            et en particulier en Bourgogne Franche-Comté, je suis reconnue pour
+            mon professionnalisme, mon sens aigu du service et des relations
+            humaines.
+          </p>
+          <p>
+            Aujourd’hui, je mets mon savoir-faire à votre service en créant
+            Marli Immobilier. Avec audace et efficacité je vous propose des
+            services à la mesure de vos attentes.
+          </p>
+          <p>
+            Comme dans toutes les fonctions que j’ai eues à occuper jusqu’ici,
+            je ne lésinerai pas sur les moyens pour mener à bien les missions
+            que vous me confierez.
+          </p>
+          <p>
+            Je me suis entourée de personnes expertes dans leur domaine
+            d’activité pour vous garantir un accompagnement optimal. Merci de
+            votre confiance.
+          </p>
         </div>
       </div>
 
@@ -49,54 +49,51 @@ function A_Propos() {
 
         <div className={styles.textContent}>
           <p className={styles.borderStyle}></p>
-          Chez Marli, la dimension humaine inhérente à toute transaction
-          immobilière est à sa juste place.
-          <br />
-          <br />
-          Nous souhaitons avoir le privilège de vous accompagner dans ces
-          moments de vie et partager vos émotions le long de votre projet
-          immobilier.
-          <br />
-          <br />
-          Nous le ferons passionnément, consciencieusement, avec transparence.
-          <br />
-          <br />
-          Nous avons une appétence particulière pour les biens d’ expressions,
-          de caractères, atypiques.
-          <br />
-          <br />
-          Nous savons aussi que tout lieu à son vécu, son histoire voir ses
-          secrets, quelque chose à exprimer. Nous aurons à cœur de vous aider à
-          les révéler.
-          <br />
-          <br />
-          Par conséquent, quelle que soit sa taille, son prix, sa situation
-          géographique, nous réservons à chaque bien une attention particulière,
-          des services sur mesure.
-          <br />
-          <br />
-          Voilà ce que nous faisons de mieux.
-          <br />
-          <br />
-          Notre moteur au quotidien sera de trouver des acquéreurs, des lieux de
-          vie qui vous ressemblent ou tout simplement des investissements
-          rentables.
-          <br />
-          <br />
-          La transaction immobilière peut être source de stress, transformons la
-          en une expérience sereine, un parcours plaisant.
-          <br />
-          <br />
-          Libérez vous l’esprit en vous reposant sur une agence à votre écoute.
-          <br />
-          <br />
-          Bienvenue chez Marli où votre satisfaction est notre première
-          gratitude.
-          <br />
-          <br />
-          A vous d’inscrire la suite de l’ histoire.
-          <br />
-          <br />
+          <p>
+            Chez Marli, la dimension humaine inhérente à toute transaction
+            immobilière est à sa juste place.
+          </p>
+          <p>
+            Nous souhaitons avoir le privilège de vous accompagner dans ces
+            moments de vie et partager vos émotions le long de votre projet
+            immobilier.
+          </p>
+          <p>
+            Nous le ferons passionnément, consciencieusement, avec transparence.
+          </p>
+          <p>
+            Nous avons une appétence particulière pour les biens d’ expressions,
+            de caractères, atypiques.
+          </p>
+          <p>
+            Nous savons aussi que tout lieu à son vécu, son histoire voir ses
+            secrets, quelque chose à exprimer. Nous aurons à cœur de vous aider
+            à les révéler.
+          </p>
+          <p>
+            Par conséquent, quelle que soit sa taille, son prix, sa situation
+            géographique, nous réservons à chaque bien une attention
+            particulière, des services sur mesure.
+          </p>
+          <p>Voilà ce que nous faisons de mieux.</p>
+          <p>
+            Notre moteur au quotidien sera de trouver des acquéreurs, des lieux
+            de vie qui vous ressemblent ou tout simplement des investissements
+            rentables.
+          </p>
+          <p>
+            La transaction immobilière peut être source de stress, transformons
+            la en une expérience sereine, un parcours plaisant.
+          </p>
+          <p>
+            Libérez vous l’esprit en vous reposant sur une agence à votre
+            écoute.
+          </p>
+          <p>
+            Bienvenue chez Marli où votre satisfaction est notre première
+            gratitude.
+          </p>
+          <p>A vous d’inscrire la suite de l’ histoire.</p>
         </div>
       </div>
     </div>
