b339390964d8aa3679cf2d360db6df1cb9e836c6
Marliimmo <marli.immobilier@gmail.com>
2025-10-23 15:01:53 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,67 +1,35 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 
-// composants de pages
+// pages
 import HomePage from './pages/Accueil/Accueil'
 import Acheter from './pages/Acheter/Acheter'
 import Vendre from './pages/Vendre/Vendre'
-import AvisDeRecherche from './pages/Avis_de_recherche/AvisDeRecherche.js'
-import NousContacter from './pages/Nous_Contacter/NousContacter.js'
-import PageDunBien from './pages/PageDunBien/PageDunBien.js'
-import MentionsLegales from './pages/MentionsLegales/MentionsLegales.js'
-import Honoraires from './pages/Honoraires/Honoraires.js'
-import APropos from './pages/A_Propos/A_Propos.js'
+import AvisDeRecherche from './pages/Avis_de_recherche/AvisDeRecherche'
+import NousContacter from './pages/Nous_Contacter/NousContacter'
+import PageDunBien from './pages/PageDunBien/PageDunBien'
+import MentionsLegales from './pages/MentionsLegales/MentionsLegales'
+import Honoraires from './pages/Honoraires/Honoraires'
+import APropos from './pages/A_Propos/A_Propos'
 
-// page d'erreur
-import NotFound from './not-found'
-
-export const router = createBrowserRouter([
+// configuration des routes
+const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      {
-        path: '/*',
-        caseSensitive: true,
-        element: <NotFound />,
-      },
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/acheter',
-        element: <Acheter />,
-      },
-      {
-        path: '/vendre',
-        element: <Vendre />,
-      },
-      {
-        path: '/avis-de-recherche',
-        element: <AvisDeRecherche />,
-      },
-      {
-        path: '/nous-contacter',
-        element: <NousContacter />,
-        <Route path='/honoraires' element={<Honoraires />} />
-      },
-      {
-        path: '/bien/:reference',
-        element: <PageDunBien />,
-      },
-      {
-        path: '/mentions-legales',
-        element: <MentionsLegales />,
-      },
-      {
-        path: '/honoraires',
-        element: <Honoraires />,
-      },
-      {
-        path: '/a-propos',
-        element: <APropos />,
-      },
-    ],
-  },
+      { path: '/', element: <HomePage /> },
+      { path: '/acheter', element: <Acheter /> },
+      { path: '/vendre', element: <Vendre /> },
+      { path: '/avis-de-recherche', element: <AvisDeRecherche /> },
+      { path: '/nous-contacter', element: <NousContacter /> },
+      { path: '/page-dun-bien', element: <PageDunBien /> },
+      { path: '/mentions-legales', element: <MentionsLegales /> },
+      { path: '/honoraires', element: <Honoraires /> },
+      { path: '/a-propos', element: <APropos /> },
+    ]
+  }
 ])
+
+export default router
+
