2a0d126d259dcafa723cc856ae4cdf60bd94c1a4
Marliimmo <marli.immobilier@gmail.com>
2025-10-23 09:10:33 +0200

    Update Honoraires.js

--- src/pages/Honoraires/Honoraires.js
+++ src/pages/Honoraires/Honoraires.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './Honoraires.module.scss';
 
+function Honoraires() {
+  return (
+    <div className="honoraires-container">
+      <h1>Nos Honoraires</h1>
+      <p>Consultez notre tableau d'honoraires ci-dessous :</p>
+      {/* Ajoutez votre tableau ou lien ici */}
+    </div>
+  );
+}
+
+export default Honoraires;
