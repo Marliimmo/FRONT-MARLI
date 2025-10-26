ce130c408cdc9fb76bcc5d05e65674a31f456591
Marliimmo <marli.immobilier@gmail.com>
2025-10-26 21:56:48 +0100

    Update Honoraires.js

--- src/pages/Honoraires/Honoraires.js
+++ src/pages/Honoraires/Honoraires.js
@@ -3,10 +3,37 @@ import './Honoraires.module.scss';
 
 function Honoraires() {
   return (
-    <div className="honoraires-container">
-      <h1>Nos Honoraires</h1>
-      <p>Consultez notre tableau d'honoraires ci-dessous :</p>
-      <iframe src="https://drive.google.com/file/d/1HvskFlQYiabOfkzZ7VnqG_CZv8Jzd7FI/preview" width="100%" height="600px" frameBorder="0"></iframe>
+    <div className="conteneur-d-honoraires">
+      <div className="container">
+        <div className="header">
+          <h1>NOS HONORAIRES TTC</h1>
+          <p className="subtitle">calculés sur le prix de vente à la charge du vendeur</p>
+        </div>
+        
+        <div className="row">
+          &lt; 50 000 € : forfait de 5 000 €
+        </div>
+        
+        <div className="row">
+          De 50 001 € à 99 999 € : 9%
+        </div>
+        
+        <div className="row">
+          De 100 000 € à 149 999 € : 8%
+        </div>
+        
+        <div className="row">
+          De 150 000 € à 249 999 € : 7%
+        </div>
+        
+        <div className="row">
+          De 250 000 € à 349 999 € : 6%
+        </div>
+        
+        <div className="row">
+          &gt; 350 000 € : 5%
+        </div>
+      </div>
     </div>
   );
 }
