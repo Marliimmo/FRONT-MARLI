c1990539f715f2b1cdd6cf8ab597de132f4486cc
Marliimmo <marli.immobilier@gmail.com>
2025-10-27 12:55:34 +0100

    Update Honoraires.js

--- src/pages/Honoraires/Honoraires.js
+++ src/pages/Honoraires/Honoraires.js
@@ -5,34 +5,43 @@ function Honoraires() {
   return (
     <div className="conteneur-d-honoraires">
       <div className="container">
-        <div className="header">
-          <h1>NOS HONORAIRES TTC</h1>
-          <p className="subtitle">calculés sur le prix de vente à la charge du vendeur</p>
-        </div>
+        <h1>NOS HONORAIRES TTC</h1>
+        <p className="subtitle">Calculés sur le prix de vente à la charge du vendeur</p>
         
-        <div className="row">
-          &lt; 50 000 € : forfait de 5 000 €
-        </div>
-        
-        <div className="row">
-          De 50 001 € à 99 999 € : 9%
-        </div>
-        
-        <div className="row">
-          De 100 000 € à 149 999 € : 8%
-        </div>
-        
-        <div className="row">
-          De 150 000 € à 249 999 € : 7%
-        </div>
-        
-        <div className="row">
-          De 250 000 € à 349 999 € : 6%
-        </div>
-        
-        <div className="row">
-          &gt; 350 000 € : 5%
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>TRANCHE DE PRIX</th>
+              <th>HONORAIRES</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>&lt; 50 000 €</td>
+              <td>Forfait de 5 000 €</td>
+            </tr>
+            <tr>
+              <td>De 50 001 € à 99 999 €</td>
+              <td>9%</td>
+            </tr>
+            <tr>
+              <td>De 100 000 € à 149 999 €</td>
+              <td>8%</td>
+            </tr>
+            <tr>
+              <td>De 150 000 € à 249 999 €</td>
+              <td>7%</td>
+            </tr>
+            <tr>
+              <td>De 250 000 € à 349 999 €</td>
+              <td>6%</td>
+            </tr>
+            <tr>
+              <td>&gt; 350 000 €</td>
+              <td>5%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
