3e3300f97794d83dd5de52ecd003d9334b1f429b
Marliimmo <marli.immobilier@gmail.com>
2026-04-24 12:49:49 +0200

    Update Honoraires.js

--- src/pages/Honoraires/Honoraires.js
+++ src/pages/Honoraires/Honoraires.js
@@ -12,6 +12,12 @@ const Honoraires = () => {
   ]
 
   return (
+    <>
+      <Helmet>
+        <title>Honoraires agence immobilière Besançon - Tarifs transparents Marli Immobilier</title>
+        <meta name='robots' content='index, follow' />
+        <meta name='description' content='Découvrez les honoraires de Marli Immobilier à Besançon. Tarifs clairs et transparents pour la vente et achat immobilier dans le Doubs.' />
+      </Helmet>
     <div className={styles.pageContainer}>
       <div className={styles.container}>
         <div className={styles.header}>
