a9ebaae17f57d48248405bc83572b89f9728da58
Marliimmo <votre-email@exemple.com>
2026-04-25 15:54:59 +0200

    Fix: react-helmet → react-helmet-async dans Honoraires.js

--- src/pages/Honoraires/Honoraires.js
+++ src/pages/Honoraires/Honoraires.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Honoraires.module.scss'
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 const Honoraires = () => {
   const tarifs = [
     { range: '< 50 000 €', tarif: 'forfait de 5 000 €' },
